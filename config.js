47865269a5cf54842a74fd0c44436c6d1d932e21
Duxlin <96833294+Duxlin@users.noreply.github.com>
2022-05-26 02:49:32 +0100

    config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['2347082252014']
+global.premium = ['2347082252014']
+global.packname = 'Quadhir bot'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
